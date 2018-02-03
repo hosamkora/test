70c7972fb900cb2d28974964dd57d5dfcdc2be46
HosamHasan <hosamkora@gmail.com>
2018-02-04 01:34:49 +0200

    configure sqlite database model ORM

--- app/controllers/StudentsController.java
+++ app/controllers/StudentsController.java
@@ -7,6 +7,7 @@ import play.mvc.*;
 import views.html.StudentControllerViews.*;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.Set;
 
 public class StudentsController extends Controller {
@@ -16,27 +17,26 @@ public class StudentsController extends Controller {
 
     // show all students
     public Result index() {
-        Set<Student> studentsModel = Student.allStudents();
+        List<Student> studentsModel = Student.find.all();
         return ok(students.render(studentsModel));
     }
 
     // add student
     public Result addStudent() {
         Form<Student> studentForm = formFactory.form(Student.class);
-        System.out.println(Student.allStudents().size());
         return ok(add_student.render(studentForm));
     }
 
     public Result saveStudent() {
         Form<Student> studentForm = formFactory.form(Student.class).bindFromRequest();
         Student student = studentForm.get();
-        Student.addStudent(student);
-        System.out.println( Student.allStudents().size());
+        student.save();
+        System.out.println( Student.find.all().size());
         return redirect(routes.StudentsController.index());
     }
 
     public Result editStudent(Integer id) {
-        Student student = Student.findByID(id);
+        Student student = Student.find.byId(id);
         if(student == null) return notFound("Not Found 404");
         else {
             Form<Student> studentForm = formFactory.form(Student.class).fill(student);
@@ -48,19 +48,20 @@ public class StudentsController extends Controller {
         //Form<Student> studentForm = formFactory.form(Student.class).bindFromRequest();
         //Student student = studentForm.get();
         Student student  =formFactory.form(Student.class).bindFromRequest().get();
-        Student oldSudent  = Student.findByID(student.getId()) ;
+        Student oldSudent  = Student.find.byId(student.id) ;
         if(oldSudent == null)
             return notFound("Student not found");
         else {
             oldSudent.name = student.name;
             oldSudent.age = student.age ;
+            oldSudent.update();
         }
         return redirect(routes.StudentsController.index());
     }
 
     // student details
     public Result showStudentDetails(Integer id) {
-        Student student = Student.findByID(id);
+        Student student = Student.find.byId(id);
         if(student ==  null)
             return notFound("Not Found");
         else
@@ -68,11 +69,11 @@ public class StudentsController extends Controller {
     }
 
     public Result deleteStudent(Integer id) {
-        Student student = Student.findByID(id);
+        Student student = Student.find.byId(id);
         if(student ==null)
             return notFound("not found");
         else {
-            Student.removeStudent(student);
+            student.delete();
             return redirect(routes.StudentsController.index());
         }
     }
--- app/models/Student.java
+++ app/models/Student.java
@@ -3,6 +3,7 @@ package models;
 import java.util.HashSet;
 import java.util.Set;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.*;
@@ -14,65 +15,9 @@ public class Student extends Model{
     public String name;
     public Integer age;
 
-    private static Set<Student> students;
+    // Integer : is type of primary key , it can be any type as you want @Id
+    //Student : is type of Model
+    public static Finder<Integer,Student> find = new Finder<>(Student.class);
 
-    static {
-        students = new HashSet<>();
-        students.add(new Student(0,"Hosam", 21));
-        students.add(new Student(1,"Ahmed", 20));
-        students.add(new Student(2,"Rimon", 19));
-    }
 
-    public Student(){
-
-    }
-
-    public Student(Integer id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public static Set<Student> allStudents(){
-        return students;
-    }
-
-    public static Student findByID(Integer id){
-        for (Student student: students)
-            if(id.equals(student.id))
-                return student;
-        return null;
-    }
-
-    public static void addStudent(Student student){
-        students.add(student);
-    }
-
-    public static boolean removeStudent(Student student){
-        return students.remove(student);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
